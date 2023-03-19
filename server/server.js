d67633680a6872cbc57c65780ccbcd09c1bf78ff
Mamitiana <mamitiana@fulldigits.com>
2023-03-19 18:47:52 +0300

    Pre_31:11 Loading the .env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
 
-dotenv.config() 
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
