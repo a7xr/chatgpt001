b1ba1a3e7aff8e13e66e9b39c8c42f51518b7c05
Mamitiana <mamitiana@fulldigits.com>
2023-03-20 21:50:35 +0300

    Lesser is going to make the AI to be able to repeat the same answer

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.post("/", async (req, res) => {
       temperature: 0, // Higher values means the model will take more risks. 0 so AI is going to answer only it known
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1,
-      frequency_penalty: 0,
+      frequency_penalty: 0.5,  // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0,
       stop: ['"""'],
     });
