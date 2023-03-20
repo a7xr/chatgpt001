c9a42baadb39a645ce5b4fff6bd221761ca8f76e
Mamitiana <mamitiana@fulldigits.com>
2023-03-20 21:43:41 +0300

    Higher values of the temperature means the model will take more risks. 0 so AI is going to answer only it known

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.post("/", async (req, res) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0.7,
+      temperature: 0, // Higher values means the model will take more risks. 0 so AI is going to answer only it known
       max_tokens: 64,
       top_p: 1,
       frequency_penalty: 0,
